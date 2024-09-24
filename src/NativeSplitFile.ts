89e4ea1827c86beaf26f5700390c164aa5e32d52
Jesse Lawler <3246877+JesseLawler@users.noreply.github.com>
2024-09-24 16:29:20 -0700

    Update NativeSplitFile.ts

--- src/NativeSplitFile.ts
+++ src/NativeSplitFile.ts
@@ -2,7 +2,7 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
-  splitFileIntoChunks(path: String, chunkSize: number): Promise<string[]>;
+  splitFileIntoChunks(path: string, chunkSize: number): Promise<string[]>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('SplitFile');
